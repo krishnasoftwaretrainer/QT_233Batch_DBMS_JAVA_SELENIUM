a27d5fec989637a2baf3ba7115bc03139f8885c9
QAKRISHNA <krishnasoftwaretrainer@gmail.com>
2026-05-28 12:05:44 +0530

    May 28th Core Java

---
+++ Core Java/NotePad Java Programs/Addition2Num.java
@@ -0,0 +1,12 @@
+class Addition2Num
+{
+public static void main(String args[])
+{
+int a=22;
+int b=33;
+int sum;
+sum=a+b;
+System.out.print("Addition of given Two Numbers:");
+System.out.println(sum);
+}
+}
---
+++ Core Java/NotePad Java Programs/Addition3Num.java
@@ -0,0 +1,9 @@
+class Addition3Num
+{
+public static void main(String args[])
+{
+int num1=10,num2=20,num3=30;
+int sum3=num1+num2+num3;
+System.out.print("Addition of given Three Numbers: "+sum3);
+}
+}
