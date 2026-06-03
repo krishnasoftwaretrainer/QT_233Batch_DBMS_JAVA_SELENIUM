5cb502836ac8e65810ef966f5515fe0ea5c72f4b
QAKRISHNA <krishnasoftwaretrainer@gmail.com>
2026-06-03 12:03:08 +0530

    June 3rd Core Java Programs

---
+++ Core Java/CoreJavaPrograms_233_234/src/datatypes_variables/Variables.java
@@ -0,0 +1,15 @@
+package datatypes_variables;
+import java.util.Scanner;
+public class Variables {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int a=10;  //4 bytes variable declaration and initialization
+		int b=scan.nextInt(); //Varible Declaration only, value will be assigned by user at runtime
+		
+		int c=a+b;  //The local variable a may not have been initialized
+	
+		System.out.println("The sum of a and b is: "+c);}
+
+	
+}
