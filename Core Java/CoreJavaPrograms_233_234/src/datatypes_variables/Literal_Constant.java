05c7683e30af69cf2e98e3227154937aa026d7da
QAKRISHNA <krishnasoftwaretrainer@gmail.com>
2026-06-04 12:25:46 +0530

    June 4th Java Programs

---
+++ Core Java/CoreJavaPrograms_233_234/src/datatypes_variables/Literal_Constant.java
@@ -0,0 +1,24 @@
+package datatypes_variables;
+
+public class Literal_Constant {
+
+	public static void main(String[] args) 
+	{
+	
+		final int htno=35  //35
+		//int htno=102;
+		final String sname="Krishna";
+		final char gender='F';
+		//gender='M';
+		
+		
+		//htno=30;
+		System.out.println(htno);
+		//int is datatype predefine[Integer]
+		//htno is Identifier and Variable Name[User Define]
+		//101:Constant Integer Literal
+		
+		
+	}
+
+}
