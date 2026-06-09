026f22247195914f2fe4df45f00aa4a117bbf6c0
QAKRISHNA <krishnasoftwaretrainer@gmail.com>
2026-06-09 12:18:49 +0530

    June 9th Java Programs

---
+++ Core Java/CoreJavaPrograms_233_234/src/operators/Assignment_Operators.java
@@ -0,0 +1,30 @@
+package operators;
+
+public class Assignment_Operators {
+
+	public static void main(String[] args) 
+	{
+		
+		int a=3,b=3,c=3,d=3,e=3;
+		
+		System.out.println(a+=5);  //a=a+5;a=8 Print:8
+		System.out.println(b-=5);  //b=b-5;b=-2
+		System.out.println(c*=5);  //c=c*5;c=15
+		System.out.println(d/=5);  //d=d/5;d=0
+		System.out.println(e%=5);  //e=e%5;3%5;e=3
+		
+		
+		
+		
+		/*
+		int x=3;  //Normal Variable 
+		
+		System.out.println(x+=5);  //x=x+5;x=3+5;x=8
+		System.out.println(x-=5);  //x=x-5;x=8-5;x=3
+		System.out.println(x*=5);  //x=15;x*5;x=3*5;x=15
+		System.out.println(x/=5);  //x=0;15/5;x=3
+		System.out.println(x%=5);  //x=3;3%5=3
+		*/
+	}
+
+}
---
+++ Core Java/CoreJavaPrograms_233_234/src/operators/Unary_Operators.java
@@ -0,0 +1,59 @@
+package operators;
+
+public class Unary_Operators {
+
+	public static void main(String[] args) 
+	{
+	
+		//int k=+10,l=-10;
+		//System.out.println(k);
+		//System.out.println(l);
+		/*
+		int a=10,b=10,c=10,d=10;
+		
+		System.out.println("Actuval a="+a); //a=10
+		System.out.println("++a="+(++a)); //a=11
+		System.out.println("Updated a="+a); //a=11
+		System.out.println("=================");
+		
+		System.out.println("Actuval b="+b);	//b=10
+		System.out.println("b++="+(b++)); //b=10
+		System.out.println("Updated b="+b); //b=11
+		System.out.println("=================");
+		
+		System.out.println("Actuval c="+c);	 //c=10	
+		System.out.println("--c="+(--c)); //c=9
+		System.out.println("Updated c="+c); //c=9
+		System.out.println("=================");
+		
+		System.out.println("Actuval d="+d); //d=10
+		System.out.println("d--="+(d--)); //d=10
+		System.out.println("Updated d="+d); //d=9
+		*/
+		
+		int x=10;
+		
+		System.out.println("First X Value:"+x);
+		System.out.println("Actuval x="+x); //a=10
+		System.out.println("++x="+(++x)); //a=11
+		System.out.println("Updated x="+x); //a=11
+		System.out.println("=================");
+		
+		System.out.println("Actuval x="+x);	//b=10
+		System.out.println("x++="+(x++)); //b=10
+		System.out.println("Updated x="+x); //b=11
+		System.out.println("=================");
+		
+		System.out.println("Actuval x="+x);	 //c=10	
+		System.out.println("--x="+(--x)); //c=9
+		System.out.println("Updated x="+x); //c=9
+		System.out.println("=================");
+		
+		System.out.println("Actuval x="+x); //d=10
+		System.out.println("x--="+(x--)); //d=10
+		System.out.println("Updated x="+x); //d=9
+		
+		System.out.println("Last X Value:"+x);
+	}
+
+}
